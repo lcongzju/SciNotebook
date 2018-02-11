dff7d174d049b8cfdc28860c96b54a16a244dede
Ziwei He <lcongzju@gmail.com>
2018-02-11 16:46:44 -0500

    login window demo

--- Main.py
+++ Main.py
@@ -2,11 +2,25 @@ import kivy
 kivy.require('1.0.6') # replace with your current kivy version !
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 
-class MyApp(App):
+class LoginScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)  # handle named arguments
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
+
+
+class MyApp(App):  # inheritance from base class App
     def build(self):
-        return Label(text='Hello world')
+        return LoginScreen()  # return your Root Widget
     
 if __name__ == '__main__':
     MyApp().run()
-    
